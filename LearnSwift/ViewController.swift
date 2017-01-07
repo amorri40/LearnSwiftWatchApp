639c2d0a6e81a5513345c71db627a0c4bdec0482
Alasdair Morrison <amorri40@gmail.com>
2017-01-07 15:07:26 +0000

    Initial Commit

---
+++ LearnSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LearnSwift
+//
+//  Created by Alasdair Morrison on 07/01/2017.
+//  Copyright © 2017 Alasdair Morrison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
