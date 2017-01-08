583ce4a6a39318f031a4a41922be5f752d0386c0
Alasdair Morrison <amorri40@gmail.com>
2017-01-08 17:31:04 +0000

    Added basic complication

--- LearnSwift WatchKit Extension/ComplicationController.swift
+++ LearnSwift WatchKit Extension/ComplicationController.swift
@@ -29,28 +29,53 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
         handler(.showOnLockScreen)
     }
     
+    func createComplicationTemplate() -> CLKComplicationTemplate {
+        let template = CLKComplicationTemplateModularLargeTable()
+        
+        template.headerTextProvider = CLKSimpleTextProvider(text: "Help me over coffee")
+        template.row1Column2TextProvider = CLKSimpleTextProvider(text: "NEW")
+        template.row1Column1TextProvider = CLKSimpleTextProvider(text: "Lucy (Java)")
+        template.row2Column2TextProvider = CLKTimeTextProvider(date: Date())
+        template.row2Column1TextProvider = CLKSimpleTextProvider(text: "Peter (JS)")
+        template.column2Alignment = CLKComplicationColumnAlignment.leading
+        return template;
+    }
+    
     // MARK: - Timeline Population
     
     func getCurrentTimelineEntry(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimelineEntry?) -> Void) {
         // Call the handler with the current timeline entry
-        handler(nil)
-    }
-    
-    func getTimelineEntries(for complication: CLKComplication, before date: Date, limit: Int, withHandler handler: @escaping ([CLKComplicationTimelineEntry]?) -> Void) {
-        // Call the handler with the timeline entries prior to the given date
-        handler(nil)
-    }
-    
-    func getTimelineEntries(for complication: CLKComplication, after date: Date, limit: Int, withHandler handler: @escaping ([CLKComplicationTimelineEntry]?) -> Void) {
-        // Call the handler with the timeline entries after to the given date
-        handler(nil)
+        //handler(nil)
+        
+        let timelineentry = CLKComplicationTimelineEntry()
+        timelineentry.complicationTemplate = createComplicationTemplate()
+        timelineentry.date = Date()
+        handler(timelineentry)
     }
     
     // MARK: - Placeholder Templates
     
     func getLocalizableSampleTemplate(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTemplate?) -> Void) {
         // This method will be called once per supported complication, and the results will be cached
-        handler(nil)
+        let complication_family = complication.family
+//        if complication_family != CLKComplicationFamily.modularLarge {
+//            handler(nil);
+//            return;
+//        }
+        
+        let template = CLKComplicationTemplateModularLargeTallBody()
+        template.headerTextProvider = CLKSimpleTextProvider(text: "Ali2")
+        template.bodyTextProvider = CLKSimpleTextProvider(text: "hello2")
+        print(complication_family.rawValue, template.description)
+        handler(createComplicationTemplate())
     }
+    
+//    func getPlaceholderTemplate(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTemplate?) -> Void) {
+//        // This method will be called once per supported complication, and the results will be cached
+//        let template = CLKComplicationTemplateModularLargeTallBody()
+//        template.headerTextProvider = CLKSimpleTextProvider(text: "Ali")
+//        template.bodyTextProvider = CLKSimpleTextProvider(text: "Hello world")
+//        handler(template)
+//    }
     
 }
